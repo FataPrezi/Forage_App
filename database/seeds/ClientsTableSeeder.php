aa417ad315929233619fec8de97c85607e0b1cb5
unknown <diabangtafa@gmail.com>
2019-06-15 14:51:42 +0000

    factory et seeders ok

--- database/seeds/ClientsTableSeeder.php
+++ database/seeds/ClientsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Gen;
 
 class ClientsTableSeeder extends Seeder
 {
@@ -16,7 +17,7 @@ class ClientsTableSeeder extends Seeder
         factory(App\Client::class,5)->create()->each(function($client,$key) use($generator){
             $id_compteur=factory(App\Compteur::class)->create()->id;
             $id_abonnemnt=factory(App\Abonnement::class)->create(["clients_id"=>$client->id,"compteurs_id"=>$id_compteur])->id;
-            $facture=factory(App\Facture::class,5)->create()->each(function($facture,$k) use($id_compteur){
+            $facture=factory(App\Facture::class,5)->create(["montant"=>$generator->numberBetween(1000,1000000)])->each(function($facture,$k) use($id_compteur){
                 factory(App\Consommation::class,10)->create(["compteurs_id"=>$id_compteur,"factures_id"=>$facture->id]);
                 factory(App\Reglement::class,10)->create(["factures_id"=>$facture->id,"montant"=>$facture->montant]);
             });
